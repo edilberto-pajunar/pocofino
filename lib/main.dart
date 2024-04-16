31a067528a7010d0df1a5b7fa5e6904af6d1cb00
eunhachingu <espajunarjr@gmail.com>
2024-04-16 22:07:45 +0800

    Add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pocofino/config/router/router.dart';
 import 'package:pocofino/config/strings/color.dart';
+import 'package:pocofino/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
